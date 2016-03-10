e3b15fb3cd0648eab0258b5f2d7224afd06ca27d
luzc08 <calebluzc08@gmail.com>
2016-03-10 01:13:12 -0500

    update trainAZ

--- train_AZ.py
+++ train_AZ.py
@@ -6,17 +6,29 @@ sys.path.insert(0, 'libsvm/python/')
 #from libsvm.python.svmutil import *
 from svmutil import *
 
-model = skipthoughts.load_model()
+if sys.argv[1]=='load':
+    model = skipthoughts.load_model()
 
-sentX = test_sentence()
+    #sentX = test_sentence()
 
-train_sentences = pickle.load( open( "sentences.p", "rb" ))
-train_label = pickle.load( open( "labels.p", "rb" ))
+    train_sentences = pickle.load( open( "sentences.p", "rb" ))
+    train_label = pickle.load( open( "labels.p", "rb" ))
 
-print train_sentences
-print train_label
+#print train_sentences
+#print train_label
 
-train_vectors = skipthoughts.encode(model,train_sentences)
+    train_vectors = skipthoughts.encode(model,train_sentences)
 
-AZmodel = svm_train(train_label,train_sentences)
-svm_save_model('AZ.model', AZmodel)
+    pickle.dump( train_vectors, open("vectors.p", "wb"))
+
+if sys.argv[1]=='train':
+    model = skipthoughts.load_model()
+
+    train_vectors = pickle.load( open( "vectors.p", "rb" ) )
+
+    train_vectors = skipthoughts.encode(model,train_sentences)
+    AZmodel = svm_train(train_label,train_vectors)
+    svm_save_model('AZ.model', AZmodel)
+
+if sys.argv[1]=='predict':
+    model = skipthoughts.load_model()
