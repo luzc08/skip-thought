b423dd3b95885bbe73d614fe239d70abd0210d24
Zhicong Lu <calebluzc08@gmail.com>
2016-03-21 14:48:33 -0400

    Revert "debug"
    This reverts commit 567dd4024333030dd1c28a0a1ac1ffd43ac71b2a.

--- predict_AZ_LR.py
+++ predict_AZ_LR.py
@@ -33,6 +33,5 @@ def preprocess(text):
 if __name__ == "__main__":
     test0 = "New information technologies are changing the way we do science. One promising new development in empirical research methodology is the emergence of online experimentation. Compared to their offline equivalents, online experiments are more customizable, more scalable, and allow for much wider and more diverse participation beyond traditionally WEIRD samples. They allow for designs that minimize experimenter effects, promote replicable methods, and lower the cost of conducting rigorous empirical research at a large scale. Modifying or repurposing existing experimental settings is much easier online, as is the open sharing of technologies, standards, and protocols. "
     sent = preprocess(test0)
-    print sent
-    #model = skipthoughts.load_model()
-    #k = predict_AZ(model, sent)
+    model = skipthoughts.load_model()
+    k = predict_AZ(model, sent)
