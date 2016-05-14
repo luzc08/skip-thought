91b0999edd34498d7a6e1887ad56fad6c94fcd9c
luzc08 <calebluzc08@gmail.com>
2016-05-14 15:07:42 -0400

    debugging

--- corpus_process/xml_to_txt.py
+++ corpus_process/xml_to_txt.py
@@ -13,18 +13,20 @@ def get_raw_text(filename, t_filename):
 
     sections = root.findall("./section")
 
-    print sections
+    #print sections
 
     for sec in sections:
-        print sec.get('heading')
+        #print sec.get('heading')
         if sec.get('heading')=='ABSTRACT':
             f.write(sec.get('heading')+'\n')
             abstract = sec.find("./para")
+            print abstract.get('text')
             f.write(abstract.get('text')+'\n')
         elif 'REFERENCE' not in sec.get('heading'):
             f.write(sec.get('heading')+'\n')
             paras = sec.findall("./para")
             for para in paras:
+                print para.get('text')
                 f.write(para.get('text')+'\n')
 
     #print paper
