659b0369a5020c1ae3f13d63fbdf50fb44672e30
luzc08 <calebluzc08@gmail.com>
2016-05-15 13:17:50 -0400

    update path

--- word2vec_model/test_w2v.py
+++ word2vec_model/test_w2v.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-model = word2vec.Word2Vec.load_word2vec_format('/tmp/vectors.bin', binary=True)
+model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
 
 model.most_similar(['girl', 'father'], ['boy'], topn=3)
 
