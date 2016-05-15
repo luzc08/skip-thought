28c547a7c6a2317da8098bd0058d23549a59c13f
luzc08 <calebluzc08@gmail.com>
2016-05-15 15:53:01 -0400

    show all test info

--- word2vec_model/test_w2v.py
+++ word2vec_model/test_w2v.py
@@ -3,13 +3,15 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
-#model = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
-#model = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
+model1 = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+model2 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
+model3 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
 
 #model.accuracy('word2vec_model/q_w.txt')
 #model.accuracy('word2vec_model/q_p.txt')
 
-print model.most_similar(['tangible', 'interaction'], ['hardware'], topn=10)
+print model1.most_similar(['tangible', 'interaction'], ['hardware'], topn=10)
+print model2.most_similar(['tangible', 'interaction'], ['hardware'], topn=10)
+print model3.most_similar(['tangible', 'interaction'], ['hardware'], topn=10)
 
 #print model.doesnt_match("breakfast cereal dinner lunch".split())
