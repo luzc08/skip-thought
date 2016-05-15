333a8583455bfa05c9fbf7efe445c661a3bbb2f3
luzc08 <calebluzc08@gmail.com>
2016-05-15 14:58:05 -0400

    eval

--- word2vec_model/test_w2v.py
+++ word2vec_model/test_w2v.py
@@ -3,8 +3,11 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+#model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+model = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
 
-print model.most_similar(['girl', 'father'], ['boy'], topn=3)
+model.accuracy('q_w.txt')
 
-print model.doesnt_match("breakfast cereal dinner lunch".split())
+#print model.most_similar(['girl', 'father'], ['boy'], topn=3)
+
+#print model.doesnt_match("breakfast cereal dinner lunch".split())
