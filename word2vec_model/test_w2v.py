2321893e781260a71fb52648abc27012f6c583c5
luzc08 <calebluzc08@gmail.com>
2016-05-15 15:41:51 -0400

    test with google pertained

--- word2vec_model/test_w2v.py
+++ word2vec_model/test_w2v.py
@@ -3,12 +3,13 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-#model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
-model = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
+model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+#model = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
+#model = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
 
 #model.accuracy('word2vec_model/q_w.txt')
 #model.accuracy('word2vec_model/q_p.txt')
 
-print model.most_similar(['tangible', 'interaction'], ['software'], topn=10)
+print model.most_similar(['tangible', 'interaction'], ['hardware'], topn=10)
 
 #print model.doesnt_match("breakfast cereal dinner lunch".split())
