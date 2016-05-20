8f0d7481d878beca55beb23916fd5c0e980ffeb7
luzc08 <calebluzc08@gmail.com>
2016-05-20 11:12:58 -0400

    revise test

--- word2vec_model/test_w2v.py
+++ word2vec_model/test_w2v.py
@@ -3,15 +3,15 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-model1 = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+#model1 = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
 model2 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
 model3 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
 
 #model.accuracy('word2vec_model/q_w.txt')
 #model.accuracy('word2vec_model/q_p.txt')
 
-print model1.most_similar(['cross', ' device'], [], topn=10)
-print model2.most_similar(['cross', 'device'], [], topn=10)
-print model3.most_similar(['cross', 'device'], [], topn=10)
+#print model1.most_similar(['cross', ' device'], [], topn=10)
+print model2.most_similar(['cross-device', 'interaction'], [], topn=10)
+print model3.most_similar(['cross-device', 'interaction'], [], topn=10)
 
 #print model.doesnt_match("breakfast cereal dinner lunch".split())
