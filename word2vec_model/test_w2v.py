84c5d9accbf85dd5a8b3168d6938c30666c9c7dd
Zhicong Lu <calebluzc08@gmail.com>
2016-06-02 13:03:06 -0400

    w2v_test

--- word2vec_model/test_w2v.py
+++ word2vec_model/test_w2v.py
@@ -1,17 +1,28 @@
 from gensim.models import word2vec
 import logging
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    model1 = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+    model2 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
+    model3 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
 
-#model1 = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
-model2 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
-model3 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
+    while True:
+        x = raw_input('Please enter 1st word to test:')
+        #test0 = [x]
+        y = raw_input('2nd word:')
 
-#model.accuracy('word2vec_model/q_w.txt')
-#model.accuracy('word2vec_model/q_p.txt')
+        pos = [x,y]
 
-#print model1.most_similar(['cross', ' device'], [], topn=10)
-print model2.most_similar(['cross-device', 'interaction'], [], topn=10)
-print model3.most_similar(['cross-device', 'interaction'], [], topn=10)
+        z = raw_input('word to substract:')
 
-#print model.doesnt_match("breakfast cereal dinner lunch".split())
+        neg = [z]
+
+    #model.accuracy('word2vec_model/q_w.txt')
+    #model.accuracy('word2vec_model/q_p.txt')
+
+        print model1.most_similar(pos,neg, topn=10)
+        print model2.most_similar(pos,neg, topn=10)
+        print model3.most_similar(pos,neg, topn=10)
+
+    #print model.doesnt_match("breakfast cereal dinner lunch".split())
