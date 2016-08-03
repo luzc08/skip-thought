4aa6e0d45ecd0975ba555c7e66b43774a74b8241
Zhicong Lu <calebluzc08@gmail.com>
2016-08-02 23:46:57 -0400

    batch processing

--- corpus_process/get_tfidf.py
+++ corpus_process/get_tfidf.py
@@ -10,10 +10,13 @@ import string
 from collections import Counter
 # from pre_process_para import stem_tokens,sent_tokenize_text
 import cPickle as pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 data_path = '/data2/luzhc/raw_text/'
 
 target_path = '/data2/luzhc/tfidf/'
+
+token_dict = {}
 
 all_tokens = []
 
@@ -38,26 +41,40 @@ def sent_tokenize_text(line):
     sentences = sent_detector.tokenize(line)
     return sentences
 
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
+
 def collect_text(filename):
     f_name = os.path.join(data_path, filename)
     print 'processing', f_name
-    tokens = []
+    # tokens = []
     if not os.path.isfile(f_name):
-        return tokens
-    for line in open(f_name):
-        print line
-        sentences = sent_tokenize_text(line)
-        for t_sent in sentences:
-            # cnt += 1
-            sent = re.sub(p, '', t_sent)
-            tokens = word_tokenize(sent)
-            tokens = [i.lower() for i in tokens if i not in string.punctuation]
-            stems = stem_tokens(tokens, stemmer)
+        return []
+
+    with open(f_name, 'r') as text_file:
+        text = text_file.read()
+        lowers = text.lower()
+        lowers1 = re.sub(p, '', lowers)
+            # remove the punctuation using the character deletion step of translate
+        no_punctuation = lowers1.translate(None, string.punctuation)
+        # tokens = nltk.word_tokenize(no_punctuation)
+        return no_punctuation
+    # for line in open(f_name):
+    #     print line
+    #     sentences = sent_tokenize_text(line)
+    #     for t_sent in sentences:
+    #         # cnt += 1
+    #         sent = re.sub(p, '', t_sent)
+    #         tokens = word_tokenize(sent)
+    #         tokens = [i.lower() for i in tokens if i not in string.punctuation]
+    #         stems = stem_tokens(tokens, stemmer)
             # if cnt%10 == 0:
             #     print stems
 
-    tokens = tokens + stems
-    return tokens
+    # tokens = tokens + stems
+    # return tokens
             # all_tokens = all_tokens + stems
             # new_line = ' '.join(stems)
             # new_line = ' '.join(tokens)
@@ -71,13 +88,18 @@ if not os.path.exists(target_path):
 t_folders = os.listdir(target_path)
 
 for filename in folders:
-    tmp = collect_text(filename)
-    if tmp:
-        all_tokens = all_tokens + tmp
+    token_dict[filename] = collect_text(filename)
+    # tmp = collect_text(filename)
+    # if tmp:
+    #     all_tokens = all_tokens + tmp
 
-pickle.dump( all_tokens, open( target_path+"tokens.p", "wb" ) )
-count = Counter(all_tokens)
-print count.most_common(100)
+tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+tfs = tfidf.fit_transform(token_dict.values())
 
+pickle.dump( token_dict, open( target_path+"token_dict.p", "wb" ) )
+pickle.dump( token_dict, open( tfs+"tfidf.p", "wb" ) )
 
-
+feature_names = tfidf.get_feature_names()
+print feature_names
+# count = Counter(all_tokens)
+# print count.most_common(100)
