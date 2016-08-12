2e896a6c6dbd287383aa2f475cb844bb7b643d4d
Zhicong Lu <calebluzc08@gmail.com>
2016-08-12 11:52:30 -0400

    bug fixed

--- word2vec_model/find_sim_w2v.py
+++ word2vec_model/find_sim_w2v.py
@@ -3,7 +3,7 @@ import logging
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    model = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
+    model1 = word2vec.Word2Vec.load_word2vec_format('/data2/luzhc/GoogleNews-vectors-negative300.bin', binary=True)
     #model2 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/test.model')
     #model3 = word2vec.Word2Vec.load('/data2/luzhc/w2v_data/mixed.model')
 
@@ -14,10 +14,11 @@ if __name__ == "__main__":
         if x == '':
             x = raw_input('Please enter word to test:')
 
+        pos = [x];
     #model.accuracy('word2vec_model/q_w.txt')
     #model.accuracy('word2vec_model/q_p.txt')
 
-        print model.similar_by_word(x, topn=10)
+        print model1.most_similar(pos, [], topn=10)
         #print model2.most_similar(pos,neg, topn=10)
         #print model3.most_similar(pos,neg, topn=10)
 
